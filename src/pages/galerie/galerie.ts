d814c5346b50628331ce71d94835e728ed7db225
Matthieu Nourry <matthieu.nourry@hotmail.fr>
2018-03-21 18:12:32 +0100

    melange

--- src/pages/galerie/galerie.ts
+++ src/pages/galerie/galerie.ts
@@ -20,13 +20,15 @@ export class GaleriePage {
 	galerie: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
-  		
-  			// Recuperation des informations de la gallerie
+
+  			// Recuperation des informations de la galerie
   		this.http.get('http://www.sebastien-thon.fr/cours/M4104Cip/projet/index.php?login=classe1&mdp=mdp1')
 		 .map(res => res.json())
 		 .subscribe(data => {
 		this.galerie = data.galeries;
 		});
+
+     console.log(this.galerie);
 
   }
 
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-list',
@@ -7,17 +9,33 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class ListPage {
 
-  	constructor(public navCtrl: NavController, public navParams: NavParams) {
-   
-  	}
-  	
-	doRefresh(refresher) {
-    	console.log('Begin async operation', refresher);
+  articles : any;
+  items: any;
 
-    	setTimeout(() => {
-      		console.log('Async operation has ended');
-      		refresher.complete();
-    	}, 2000);
-  	}
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
+    this.http.get('http://www.sebastien-thon.fr/cours/M4104Cip/projet/index.php?login=classe1&mdp=mdp1')
+     .map(res => res.json())
+     .subscribe(data => {
+    this.articles = data.articles;
+    });
+     console.log(this.articles);
+
+  }
+
+  Initialisation() {
+    this.items = this.articles;
+  }
+
+  Input(event){
+    this.Initialisation();
+
+    let val = event.target.value;
+/*
+    if(val && val.trim() != '') {
+      this.items= this.items.filter((items) => {
+        return (items.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }*/
+  }
 
 }

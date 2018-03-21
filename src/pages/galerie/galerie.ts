2a48e691fc899f329978550e2b1379d8ed764cb2
simonjayet <34447091+simonjayet@users.noreply.github.com>
2018-03-21 18:01:53 +0100

    add

--- src/pages/galerie/galerie.ts
+++ src/pages/galerie/galerie.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
 
 /**
  * Generated class for the GaleriePage page.
@@ -14,21 +17,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'galerie.html',
 })
 export class GaleriePage {
+	galerie: any;
 
-	constructor(public navCtrl: NavController, public navParams: NavParams) {
-	}
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
+  		
+  			// Recuperation des informations de la gallerie
+  		this.http.get('http://www.sebastien-thon.fr/cours/M4104Cip/projet/index.php?login=classe1&mdp=mdp1')
+		 .map(res => res.json())
+		 .subscribe(data => {
+		this.galerie = data.galeries;
+		});
 
-	ionViewDidLoad() {
-	    console.log('ionViewDidLoad GaleriePage');
-	}
+  }
 
-  	doRefresh(refresher) {
-    	console.log('Begin async operation', refresher);
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad GaleriePage');
+  }
 
-    	setTimeout(() => {
-      		console.log('Async operation has ended');
-      		refresher.complete();
-    	}, 2000);
-  	}
+
 
 }

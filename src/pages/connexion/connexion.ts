a846ed0bbde574d12dd0084f845d12d8170ed1e3
Matthieu Nourry <matthieu.nourry@hotmail.fr>
2018-03-21 18:51:39 +0100

    connexion OK

--- src/pages/connexion/connexion.ts
+++ src/pages/connexion/connexion.ts
@@ -19,7 +19,7 @@ import 'rxjs/add/operator/map';
 })
 export class ConnexionPage {
   resultats: any;
-  connexion = {}
+  connexion = {};
   constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http, private storage: Storage) {
     
   }
@@ -31,13 +31,16 @@ export class ConnexionPage {
 
   connexionOK() {
     console.log(this.connexion);
-    this.http.get('http://www.sebastien-thon.fr/cours/M4104Cip/projet/index.php?connexion&login={{connexion.login}}&mdp={{connexion.password}}')
+    this.http.get('http://www.sebastien-thon.fr/cours/M4104Cip/projet/index.php?connexion&login=' + this.connexion.login + '&mdp=' + this.connexion.password)
       .map(res => res.json())
       .subscribe(data => {
         this.resultats = data;
         console.log(this.resultats);
+        if(this.resultats.resultat=='OK')
+          this.navCtrl.setRoot(HomePage);
+        else
+          console.log('echec critique');
     });
-    //this.navCtrl.push(HomePage);
   }
 
 }
